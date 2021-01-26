a4da9a3e8069f8fc0dc9fd11f5dfa698fb77d4fe
v0idpwn <v0idpwn@gmail.com>
2021-01-25 22:06:10 -0300

    Limit amount of bins in the page

--- lib/spellpaste_web/live/page_live.ex
+++ lib/spellpaste_web/live/page_live.ex
@@ -6,17 +6,19 @@ defmodule SpellpasteWeb.PageLive do
   alias Spellpaste.Pastes
   alias Spellpaste.Pastes.Bin
 
+  @limit 5
+
   defp pubsub_channel, do: Application.get_env(:spellpaste, :pubsub_channel)
 
   @impl true
   def mount(_params, _session, socket) do
     PubSub.subscribe(pubsub_channel(), Events.BinCreated.topic())
 
-    {:ok, assign(socket, bins: Pastes.last_bins(5))}
+    {:ok, assign(socket, bins: Pastes.last_bins(@limit))}
   end
 
   @impl true
   def handle_info(%Bin{} = bin, socket) do
-    {:noreply, assign(socket, bins: [bin | socket.assigns[:bins]])}
+    {:noreply, assign(socket, bins: [bin | Enum.take(socket.assigns[:bins], @limit - 1)])}
   end
 end
