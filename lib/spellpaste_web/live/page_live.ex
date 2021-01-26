c7e898a237b3ab56d09abce8e56b5e6551a38192
v0idpwn <v0idpwn@gmail.com>
2021-01-26 01:11:41 -0300

    Show stats in page_live

--- lib/spellpaste_web/live/page_live.ex
+++ lib/spellpaste_web/live/page_live.ex
@@ -13,12 +13,18 @@ defmodule SpellpasteWeb.PageLive do
   @impl true
   def mount(_params, _session, socket) do
     PubSub.subscribe(pubsub_channel(), Events.BinCreated.topic())
+    PubSub.subscribe(pubsub_channel(), Events.StatsUpdated.topic())
 
-    {:ok, assign(socket, bins: Pastes.last_bins(@limit))}
+    {:ok, assign(socket, bins: Pastes.last_bins(@limit), stats: nil)}
   end
 
   @impl true
   def handle_info(%Bin{} = bin, socket) do
     {:noreply, assign(socket, bins: [bin | Enum.take(socket.assigns[:bins], @limit - 1)])}
+  end
+
+  @impl true
+  def handle_info(%Events.StatsUpdated{} = new_stats, socket) do
+    {:noreply, assign(socket, stats: new_stats, stats_last_updated: NaiveDateTime.utc_now())}
   end
 end
